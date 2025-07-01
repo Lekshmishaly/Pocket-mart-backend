487d95979a9f95b840f58d3d5bd89a7f58132612
Lekshmi Shaly <lekshmishaly@Lekshmis-MacBook-Air.local>
2025-07-01 17:39:05 +0530

    JWT access token issue

--- Utils/genarateAccessToken.js
+++ Utils/genarateAccessToken.js
@@ -1,6 +1,6 @@
+const jwt = require("jsonwebtoken");
 require("dotenv").config;
 
-const jwt = require("jsonwebtoken");
 const generateAccessToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.ACCESS_TOKEN_KEY, {
     expiresIn: "15m",
